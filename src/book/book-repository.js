7a6b6ba41529ad0b0930ae11bf4531f475e757e2
LeVinhSang <sanglv@sphinx-software.com>
2018-03-08 09:48:11 +0700

    update edit publisher_id to not null to null

--- src/book/book-repository.js
+++ src/book/book-repository.js
@@ -34,7 +34,7 @@ class BookRepository{
         return this.connection('books').update({
             title: book.getTitle(),
             author: book.getAuthor(),
-            publisher_id: book.getPublisher().getId(),
+            publisher_id: book.getPublisher().getId() ? book.getPublisher().getId() : null,
             price: book.getPrice()
         }).where({
             id: book.getId()
